3cdea69d337bf2658f98dacd9a0467fce9e2ca3a
Ahmad Dayeh <91208330+ahmaddayeh@users.noreply.github.com>
2023-05-26 22:58:52 +0300

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -8,7 +8,7 @@ interface LogoProps {
 
 export const Logo: FunctionComponent<LogoProps> = ({ sidebar = false }) => {
     const { themeVariant } = useContext(ThemeContext);
-    const imgUrl = window.location.protocol + "//" + window.location.host + "/logo.sbg";
+    const imgUrl = window.location.protocol + "//" + window.location.host + "/logo.svg";
 
     return (
         <img src={imgUrl} width="214px" height="56px" className={`w-54 h-14 ${sidebar ? "" : "-ml-5"}`} alt="Logo IVAO Brasil" />
